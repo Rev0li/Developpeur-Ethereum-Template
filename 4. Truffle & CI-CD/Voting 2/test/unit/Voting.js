5ed56231cf9dfbe72a767a72320731f6cc47f877
Revoli <kientzler.oliver@gmail.com>
2023-06-12 12:07:32 +0200

    all cover test unitaire 100%

--- 4. Truffle & CI-CD/Voting 2/test/unit/Voting.js
+++ 4. Truffle & CI-CD/Voting 2/test/unit/Voting.js
@@ -1,7 +1,5 @@
 const { expect } = require("chai");
-const { expectEvent, ether } = require("@openzeppelin/test-helpers");
 const { ethers } = require("hardhat");
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { BigNumber } = require("ethers");
 
 describe("Contract Voting", function () {
@@ -12,156 +10,179 @@ describe("Contract Voting", function () {
     const Voting = await ethers.getContractFactory("Voting");
     votingInstance = await Voting.deploy();
     await votingInstance.deployed();
+
     [owner, alice, bob, patric] = await ethers.getSigners();
+
     providerAlice = votingInstance.connect(alice);
     providerBob = votingInstance.connect(bob);
     providerPatric = votingInstance.connect(patric);
   });
 
-  ////////////////////   FIXTURE   \\\\\\\\\\\\\\\\\\\
-  async function addWhitelist() {
-    await votingInstance.addVoter(alice.address);
-    await votingInstance.addVoter(bob.address);
-    await votingInstance.addVoter(patric.address);
-
-    return { alice, bob, patric };
-  }
-
-  async function addProposition() {
-    await votingInstance.addVoter(alice.address);
-    await votingInstance.addVoter(bob.address);
-    await votingInstance.addVoter(patric.address);
-
-    await votingInstance.startProposalsRegistering();
-
-    await providerAlice.addProposal("hello world");
-    await providerBob.addProposal("My name is Bob");
-    await providerPatric.addProposal("Moi c'est Pa pa patric");
-
-    return { alice, bob, patric };
-  }
-
   ////////////////////   REGISTRATION   \\\\\\\\\\\\\\\\\\\
 
   describe("function addVoter and getVoter", function () {
-    it("require workflowStatus RegisteringVoters", async () => {
-      // Définir l'état
+    it("should return Caller is not the owner", async () => {
+      await expect(providerAlice.addVoter(alice.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("should return Voters registration is not open yet", async () => {
       await votingInstance.startProposalsRegistering();
 
-      // Utiliser expect avec revertedWith pour vérifier si require est declanché
-      expect(votingInstance.addVoter(alice.address)).to.be.revertedWith(
+      await expect(votingInstance.addVoter(alice.address)).to.be.revertedWith(
         "Voters registration is not open yet"
       );
     });
 
-    it("require isRegistered ", async () => {
-      // Ajouter Alice en tant que votant
+    it("should return Already registered ", async () => {
       await votingInstance.addVoter(alice.address);
 
-      // Utiliser expect avec revertedWith pour vérifier si require est declanché
-      expect(votingInstance.addVoter(alice.address)).to.be.revertedWith(
+      await expect(votingInstance.addVoter(alice.address)).to.be.revertedWith(
         "Already registered"
       );
     });
 
-    it("should add a voter (alice)", async () => {
-      await votingInstance.addVoter(alice.address);
-      const voter = await providerAlice.getVoter(alice.address);
-      expect(voter.isRegistered).to.be.true;
-    });
-
-    it("check getVoter", async () => {
-      const { alice, bob } = await loadFixture(addWhitelist);
-
-      const voter = await votingInstance.connect(bob).getVoter(alice.address);
-      expect(voter.isRegistered).to.be.true; // Vérifier si le votant est enregistré
-      expect(voter.hasVoted).to.be.oneOf([true, false]);
-      expect(voter.votedProposalId).to.be.instanceOf(ethers.BigNumber);
-    });
-
-    it("emit voterRegistred", async () => {
-      // Utiliser to.emit pour vérifier si l'événement est émis
+    it("should emit a new struc in a mapping with address Voters", async () => {
       await expect(votingInstance.addVoter(alice.address))
         .to.emit(votingInstance, "VoterRegistered")
         .withArgs(alice.address);
+    });
+
+    it("should return struc of Voter with getVoters", async () => {
+      await votingInstance.addVoter(alice.address);
+      await votingInstance.addVoter(bob.address);
+
+      const voter = await votingInstance.connect(bob).getVoter(alice.address);
+      expect(await voter.isRegistered).to.be.true;
+      expect(await voter.hasVoted).to.be.false;
+      expect(await voter.votedProposalId).to.be.instanceOf(ethers.BigNumber);
+    });
+
+    it("should return You're not a voter", async () => {
+      await votingInstance.addVoter(alice.address);
+
+      await expect(votingInstance.getVoter(alice.address)).to.be.rejectedWith(
+        "You're not a voter"
+      );
     });
   });
 
   ////////////////////   PROPOSAL   \\\\\\\\\\\\\\\\\\\
   describe("function proposal and getOneProposal", function () {
-    it("require ProposalsRegistrationStarted", async () => {
-      // Ajouter Alice en tant que votant
-      await votingInstance.addVoter(alice.address);
-
-      // Utiliser expect avec revertedWith pour vérifier si require est declanché
-      expect(
-        votingInstance.connect(alice).addProposal("hello you")
-      ).to.be.revertedWith("Proposals are not allowed yet");
+    it("should return You're not a voter", async () => {
+      await votingInstance.startProposalsRegistering();
+      await expect(
+        votingInstance.addProposal("hello world")
+      ).to.be.revertedWith("You're not a voter");
     });
 
-    it("require empty", async () => {
-      // Ajouter Alice en tant que votant
+    it("should return Proposals are not allowed yet", async () => {
+      await votingInstance.addVoter(alice.address);
+
+      await expect(providerAlice.addProposal("hello you")).to.be.revertedWith(
+        "Proposals are not allowed yet"
+      );
+    });
+
+    it("should return Vous ne pouvez pas ne rien proposer", async () => {
       await votingInstance.addVoter(alice.address);
       await votingInstance.startProposalsRegistering();
 
-      // Utiliser expect avec revertedWith pour vérifier si require est declanché
-      await expect(
-        votingInstance.connect(alice).addProposal("")
-      ).to.be.revertedWith("Vous ne pouvez pas ne rien proposer");
+      await expect(providerAlice.addProposal("")).to.be.revertedWith(
+        "Vous ne pouvez pas ne rien proposer"
+      );
     });
 
-    it("should add and get a proposal ", async () => {
-      await loadFixture(addProposition);
+    it("should add and get a proposal equal ", async () => {
+      await votingInstance.addVoter(alice.address);
+      await votingInstance.addVoter(bob.address);
+      await votingInstance.addVoter(patric.address);
+
+      await votingInstance.startProposalsRegistering();
+
+      await providerAlice.addProposal("hello world");
+      await providerBob.addProposal("My name is Bob");
+      await providerPatric.addProposal("Moi c'est Pa pa patric");
 
       const proposal1 = await providerAlice.getOneProposal(1);
       const proposal2 = await providerBob.getOneProposal(2);
       const proposal3 = await providerPatric.getOneProposal(3);
-      console.log(proposal1);
-      // Vérifier si les propositions sont égal
-      expect(proposal1.description).to.equal("hello world");
-      expect(proposal2.description).to.equal("My name is Bob");
-      expect(proposal3.description).to.equal("Moi c'est Pa pa patric");
+
+      expect(await proposal1.description).to.equal("hello world");
+      expect(await proposal2.description).to.equal("My name is Bob");
+      expect(await proposal3.description).to.equal("Moi c'est Pa pa patric");
     });
 
-    it("emit ProposalRegistered", async () => {
+    it("should emit in proposalsArray a voter proposal ", async () => {
       await votingInstance.addVoter(alice.address);
       await votingInstance.startProposalsRegistering();
 
-      await expect(votingInstance.connect(alice).addProposal("hello you"))
+      await expect(providerAlice.addProposal("hello you"))
         .to.emit(votingInstance, "ProposalRegistered")
         .withArgs(1);
+    });
+
+    it("should return You're not a voter", async () => {
+      await votingInstance.addVoter(alice.address);
+      await votingInstance.startProposalsRegistering();
+      await providerAlice.addProposal("hello world");
+
+      await expect(
+        votingInstance.getOneProposal(alice.address)
+      ).to.be.rejectedWith("You're not a voter");
     });
   });
 
   ////////////////////   VOTE   \\\\\\\\\\\\\\\\\\\
 
-  describe.only("function setVote", function () {
-    it("require VotingSessionStarted", async () => {
-      loadFixture(addProposition);
+  describe("function setVote", function () {
+    beforeEach(async function () {
+      await votingInstance.addVoter(alice.address);
+      await votingInstance.addVoter(bob.address);
+      await votingInstance.addVoter(patric.address);
+
+      await votingInstance.startProposalsRegistering();
+
+      await providerAlice.addProposal("hello world");
+      await providerBob.addProposal("My name is Bob");
+      await providerPatric.addProposal("Moi c'est Pa pa patric");
+    });
+
+    it("should return You're not a voter", async () => {
+      await expect(votingInstance.setVote(2)).to.be.revertedWith(
+        "You're not a voter"
+      );
+    });
+
+    it("should return Voting session havent started yet", async () => {
       await votingInstance.endProposalsRegistering();
 
-      expect(votingInstance.connect(alice).setVote(1)).to.be.revertedWith(
+      await expect(providerAlice.setVote(2)).to.be.revertedWith(
         "Voting session havent started yet"
       );
     });
-    //PROBLEME
-    it("require already voted", async () => {
-      await loadFixture(addProposition);
+
+    it("should return You have already voted", async () => {
       await votingInstance.endProposalsRegistering();
       await votingInstance.startVotingSession();
 
       await providerBob.setVote(1);
 
-      expect(providerBob.setVote(1)).to.be.revertedWith(
+      await expect(providerBob.setVote(1)).to.be.revertedWith(
         "You have already voted"
       );
     });
 
-    it("sould return votedProposalId", async () => {});
-    //PROBLEME
-    it("should voteCount++", async () => {
-      await loadFixture(addProposition);
+    it("should return Proposal not found", async () => {
+      await votingInstance.endProposalsRegistering();
+      await votingInstance.startVotingSession();
+      await expect(providerAlice.setVote(5)).to.be.revertedWith(
+        "Proposal not found"
+      );
+    });
 
+    it("should voteCount", async () => {
       await votingInstance.endProposalsRegistering();
       await votingInstance.startVotingSession();
 
@@ -169,78 +190,185 @@ describe("Contract Voting", function () {
       await providerAlice.setVote(1);
       await providerPatric.setVote(2);
 
-      const recupProposal1 = await providerAlice.getOneProposal(1);
-      const recupProposal2 = await providerAlice.getOneProposal(2);
-      const recupProposal3 = await providerAlice.getOneProposal(3);
+      const recupProposal1 = await providerBob.getOneProposal(1);
+      const recupProposal2 = await providerBob.getOneProposal(2);
+      const recupProposal3 = await providerBob.getOneProposal(3);
 
-      expect(recupProposal1.voteCount).to.equal(ethers.BigNumber.from(2));
-      expect(recupProposal2.voteCount).to.equal(ethers.BigNumber.from(1));
-      expect(recupProposal3.voteCount).to.equal(ethers.BigNumber.from(0));
+      expect(await recupProposal1.voteCount).to.equal(ethers.BigNumber.from(2));
+      expect(await recupProposal2.voteCount).to.equal(ethers.BigNumber.from(1));
+      expect(await recupProposal3.voteCount).to.equal(ethers.BigNumber.from(0));
     });
-    //PROBLEME
-    it("should recup hasVoted true and votedProposalId", async () => {
-      await loadFixture(addProposition);
 
+    it("should recup hasVoted true and votedProposal Id and voteCount", async () => {
       await votingInstance.endProposalsRegistering();
       await votingInstance.startVotingSession();
 
       await providerAlice.setVote(1);
 
       const voter = await providerAlice.getVoter(alice.address);
-      console.log(voter);
 
-      expect(voter.hasVoted).to.be.true;
-      expect(voter.votedProposalId).to.equal(ethers.BigNumber.from(1));
+      await expect(voter.hasVoted).to.be.true;
+      await expect(voter.votedProposalId).to.equal(ethers.BigNumber.from(1));
     });
-    //PROBLEME
-    it("emit Voted", async () => {
-      await loadFixture(addProposition);
 
+    it("should emit address and voterProposalId in proposalsArray and in mapping", async () => {
       await votingInstance.endProposalsRegistering();
       await votingInstance.startVotingSession();
 
-      await expect(votingInstance.connect(alice).setVote(1))
+      await expect(providerAlice.setVote(1))
         .to.emit(votingInstance, "Voted")
         .withArgs(alice.address, 1);
     });
   });
-  ////////////////////   MODIFIER   \\\\\\\\\\\\\\\\\\\
 
-  describe("check onlyVoter and onlyOwner", function () {
-    it("check in getVoter", async () => {
-      const { alice } = await loadFixture(addWhitelist);
+  ////////////////////   TALLYVOTES   \\\\\\\\\\\\\\\\\\\
 
-      expect(votingInstance.getVoter(alice.address)).to.be.rejectedWith(
-        "You're not a voter"
+  describe("tallyVotes", async () => {
+    beforeEach(async function () {
+      await votingInstance.addVoter(alice.address);
+      await votingInstance.addVoter(bob.address);
+      await votingInstance.addVoter(patric.address);
+
+      await votingInstance.startProposalsRegistering();
+
+      await providerAlice.addProposal("hello world");
+      await providerBob.addProposal("My name is Bob");
+      await providerPatric.addProposal("Moi c'est Pa pa patric");
+
+      await votingInstance.endProposalsRegistering();
+      await votingInstance.startVotingSession();
+
+      await providerBob.setVote(1);
+      await providerAlice.setVote(1);
+      await providerPatric.setVote(2);
+    });
+
+    it("should return Ownable: caller is not the owner", async () => {
+      await votingInstance.endVotingSession();
+      await expect(providerAlice.tallyVotes()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
       );
     });
 
-    it("check in getOneProposal", async () => {
-      await loadFixture(addProposition);
-
-      expect(votingInstance.getOneProposal(1)).to.be.rejectedWith(
-        "You're not a voter"
+    it("require Voting session ended", async () => {
+      await expect(votingInstance.tallyVotes()).to.be.revertedWith(
+        "Current status is not voting session ended"
       );
+    });
+
+    it("emit WorkflowStatusChange", async () => {
+      await votingInstance.endVotingSession();
+
+      await expect(await votingInstance.tallyVotes())
+        .to.emit(votingInstance, "WorkflowStatusChange")
+        .withArgs(4, 5);
+    });
+  });
+
+  ////////////////////   STATE   \\\\\\\\\\\\\\\\\\\
+
+  describe("startProposalsRegistering", async () => {
+    it("should return Ownable: caller is not the owner", async () => {
+      await expect(
+        providerAlice.startProposalsRegistering()
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("should return Registering proposals cant be started now", async () => {
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.endProposalsRegistering();
+      await expect(
+        votingInstance.startProposalsRegistering()
+      ).to.be.revertedWith("Registering proposals cant be started now");
+    });
+
+    it("should emit WorkflowStatusChange 0 to 1", async () => {
+      await expect(await votingInstance.startProposalsRegistering())
+        .to.emit(votingInstance, "WorkflowStatusChange")
+        .withArgs(0, 1);
+    });
+  });
+
+  describe("endProposalsRegistering", async () => {
+    it("should return Ownable: caller is not the owner", async () => {
+      await expect(providerAlice.endProposalsRegistering()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("should return Registering proposals havent started yet", async () => {
+      await expect(votingInstance.endProposalsRegistering()).to.be.revertedWith(
+        "Registering proposals havent started yet"
+      );
+    });
+
+    it("should emit WorkflowStatusChange 1 to 2", async () => {
+      await votingInstance.startProposalsRegistering();
+
+      await expect(await votingInstance.endProposalsRegistering())
+        .to.emit(votingInstance, "WorkflowStatusChange")
+        .withArgs(1, 2);
+    });
+  });
+
+  describe("startVotingSession", async () => {
+    it("should return Ownable: caller is not the owner", async () => {
+      await expect(providerAlice.startVotingSession()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("should return Registering proposals phase is not finished", async () => {
+      await expect(votingInstance.startVotingSession()).to.be.revertedWith(
+        "Registering proposals phase is not finished"
+      );
+    });
+
+    it("should emit WorkflowStatusChange 2 to 3", async () => {
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.endProposalsRegistering();
+
+      await expect(await votingInstance.startVotingSession())
+        .to.emit(votingInstance, "WorkflowStatusChange")
+        .withArgs(2, 3);
+    });
+  });
+
+  describe("endVotingSession", async () => {
+    it("should return Ownable: caller is not the owner", async () => {
+      await expect(providerAlice.endVotingSession()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      );
+    });
+
+    it("should return Voting session havent started yet", async () => {
+      await expect(votingInstance.endVotingSession()).to.be.revertedWith(
+        "Voting session havent started yet"
+      );
+    });
+
+    it("should emit WorkflowStatusChange 3 to 4", async () => {
+      await votingInstance.startProposalsRegistering();
+      await votingInstance.endProposalsRegistering();
+      await votingInstance.startVotingSession();
+
+      await expect(await votingInstance.endVotingSession())
+        .to.emit(votingInstance, "WorkflowStatusChange")
+        .withArgs(3, 4);
     });
   });
 
   ////////////////////   GENESIS   \\\\\\\\\\\\\\\\\\\
   it("recup genesis", async () => {
-    // Ajouter Alice en tant que votant
     await votingInstance.addVoter(alice.address);
-
-    // Définir l'état pour permettre l'enregistrement des propositions
+    // change de status et ajoute le genesis (0)
     await votingInstance.startProposalsRegistering();
 
-    // Ajouter une instance et connect alice
     const providerAlice = votingInstance.connect(alice);
-    //Ajouter une proposal
-    await providerAlice.addProposal("hello world");
 
-    //recupere la Proposal 0 (normalement GENESIS)
     const proposal = await providerAlice.getOneProposal(0);
 
-    // Vérifier si la première proposition est égale à "hello world"
-    expect(proposal.description).to.equal("GENESIS");
+    //recupere la Proposal 0 (normalement GENESIS)
+    expect(await proposal.description).to.equal("GENESIS");
   });
 });
